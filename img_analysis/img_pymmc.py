20dfe61a04fa5c9897c22e8ddb9ac119af9eb4f7
MinTTT <pan_chu@outlook.com>
2022-05-19 17:21:43 +0800

    amend

--- img_analysis/img_pymmc.py
+++ img_analysis/img_pymmc.py
@@ -5,20 +5,19 @@ import tifffile as tiff
 import os
 from scipy.ndimage import histogram
 import sys
-import matplotlib
+# import matplotlib
+
 # matplotlib.use('TkAgg')
 
-#%%
-img_ps = r'C:\Users\pc\Desktop\streads\HDR_random_exp'
-img_names = os.listdir(img_ps)
+# %%
+img_ps = r'C:\Users\pan_c\Desktop'
+img_names = [file for file in os.listdir(img_ps) if file.split('.')[-1] == 'tiff']
 img = tiff.imread(os.path.join(img_ps, img_names[0]))
 
-img_hist = histogram(img, 0, 2**8, 1000)
-
-
+# img_hist = histogram(img, 0, np.median(img)*1.5, 5000)
 
 fig1, ax = plt.subplots(1, 1)
+density_range = (img.min(), np.median(img)*1.5)
+ax.hist(img.flatten(), bins=int(np.ptp(density_range)), range=density_range)
 
-ax.hist(img, bins=1000, range=(0, 2**8))
-
-plt.show()
+fig1.show()
